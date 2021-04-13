faf131077ae8d4ba92cce4d7e667fbdb231ad393
CodeBlooded88 <49377737+CodeBlooded88@users.noreply.github.com>
2021-04-13 13:09:12 -0400

    Customer: create initial business layer classes

---
+++ Account.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capstone_team3;
+
+/**
+ *
+ * @author Christina
+ */
+public class Account {
+
+    String accoutnNum;
+    String customerName;
+    String customerAddress;
+    String contactNumber;
+    String customerPayment;
+    String customerPickUp; //May change over course of development
+    MyCart customerOrder; //May change to database reference
+    boolean active;
+}
---
+++ MyCart.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capstone_team3;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Christina
+ */
+public class MyCart {
+    
+    ArrayList<Item> itemsInCart;
+    double price;
+    int quantity;
+    Item item;
+    
+}
